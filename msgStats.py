95751ae22c5363db978eed356c1b8778392b4104
hernerwerzog <jczykier@wp.pl>
2023-02-11 11:41:44 +0100

    Added file-handling functions

--- msgStats.py
+++ msgStats.py
@@ -1,7 +1,29 @@
 import json
+import os
 
 
 MSG_FOLDER_NAME = 'messeges'
 
+#Reads json file
+def readFile(fileName):
+    file = open(MSG_FOLDER_NAME +'/'+fileName)
+    data = json.load(file)
+    file.close()
+    return data
+
+#Returns number of files in a folder
+def getFilesNum(path):
+    files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path,f))]
+    return len(files)
+
+#Returns list of participants in a conversation
+def getParticipants(data):
+    participantsList = []
+    for person in data['participants']:
+        participantsList.append(person['name'])
+    return participantsList
+
+
 if __name__ == "__main__":
-    print("hello world")
+    data = readFile("message_1.json")
+    print(getParticipants(data))
